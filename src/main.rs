0f2c7494372e8f9195b006ef8d16754ce6ba39ac
fabien.leconte <fabien.leconte@kosmos.fr>
2018-12-11 18:18:30 +0100

    Revue de l'organisation en module

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 mod server_configuration;
 
 fn main() {
-    let configuration = server_configuration::server_configuration::init();
+    let configuration = server_configuration::init();
     println!("Hello, world!");
     println!("{:?}", configuration);
 }
--- src/server_configuration/mod.rs
+++ src/server_configuration/mod.rs
@@ -2,19 +2,16 @@ extern crate serde_derive;
 extern crate dotenv;
 extern crate envy;
 
-pub mod server_configuration {
+#[derive(serde_derive::Deserialize, Debug)]
+pub struct Configuration {
+    pub database_url: String,
+    pub temp_folder: String
+}
 
-    #[derive(serde_derive::Deserialize, Debug)]
-    pub struct Configuration {
-        pub database_url: String,
-        pub temp_folder: String
-    }
-
-    pub fn init() -> Configuration {
-        dotenv::dotenv().expect("Failed to read .env file");
-        match envy::from_env::<Configuration>() {
-            Ok(config) => config,
-            Err(e) => panic!(e)
-        }
+pub fn init() -> Configuration {
+    dotenv::dotenv().expect("Failed to read .env file");
+    match envy::from_env::<Configuration>() {
+        Ok(config) => config,
+        Err(e) => panic!(e)
     }
 }
